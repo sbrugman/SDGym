ca37c59d403db643365fd08a6e4abebc54e2f8f4
Simon Brugman <simon.brugman@ing.com>
2021-06-04 08:18:00 +0200

    fork

--- setup.py
+++ setup.py
@@ -109,6 +109,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDGym',
-    version='0.2.2',
+    version='0.2.2.fork',
     zip_safe=False,
 )
